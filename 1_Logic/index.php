5e5c0b862ac863355e064d05ee087e8bb20aaeaf
mannurramadhan <mannurramadhan@gmail.com>
2021-11-03 15:33:21 +0700

    + Logic - initial index.php and form input

---
+++ 1_Logic/index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Assessment Test - Logic</title>
+</head>
+
+<body>
+    <div>
+        <nav>
+            <div>
+                <h4>Logic Questions</h4>
+            </div>
+        </nav>
+        <div>
+            <div>
+
+            </div>
+            <div>
+                <form action="" method="POST">
+                    <div>
+                        <div>
+                            <label for="#input-number">Insert Number for Array Index:</label>
+                            <div id="input-number">
+                                <input type="number" name="index-1" />
+                                <input type="number" name="index-2" />
+                                <input type="number" name="index-3" />
+                                <input type="number" name="index-4" />
+                                <button type="button">Add</button>
+                            </div>
+                        </div>
+                        <div>
+                            <label>Insert Target Value:</label>
+                            <input type="number" name="target-value" />
+                        </div>
+                    </div>
+                    <button type="submit">Generate</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
