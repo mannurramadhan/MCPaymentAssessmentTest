7895fa503771e8f3da399b6a5c330e3264f785cb
mannurramadhan <mannurramadhan@gmail.com>
2021-11-04 00:01:05 +0700

    + Logic - set logo and less margin

--- 1_Logic/index.php
+++ 1_Logic/index.php
@@ -7,6 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Assessment Test - Logic</title>
     <link rel="stylesheet" href="style.css">
+    <link rel="shortcut icon" href="./img/logo-lp.png">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat&family=Pacifico&display=swap" rel="stylesheet">
