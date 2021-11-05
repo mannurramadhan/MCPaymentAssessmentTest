1067ac2b5508ae85b748173db33f38544888eda5
mannurramadhan <mannurramadhan@gmail.com>
2021-11-06 02:57:37 +0700

    + Budget - BE - enable CORS

--- 2_Budget_Application/backend-server/public/index.php
+++ 2_Budget_Application/backend-server/public/index.php
@@ -1,5 +1,8 @@
 <?php
 
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+
 /*
 |--------------------------------------------------------------------------
 | Create The Application
