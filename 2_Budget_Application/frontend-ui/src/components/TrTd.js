2b5cc9a24853bc0ef82df55e8277ade9ca47faf5
mannurramadhan <mannurramadhan@gmail.com>
2021-11-06 07:42:52 +0700

    + Budget - FE - set align center for td

--- 2_Budget_Application/frontend-ui/src/components/TrTd.js
+++ 2_Budget_Application/frontend-ui/src/components/TrTd.js
@@ -3,9 +3,9 @@ import "./Table.css";
 function TrTd({ tr }) {
     return (
         <tr>
-            <td>{tr.id}</td>
+            <td className="align-center">{tr.id}</td>
             <td>{tr.transaction_name}</td>
-            <td>{tr.transaction_category}</td>
+            <td className="align-center">{tr.transaction_category}</td>
             <td>{tr.income}</td>
             <td>{tr.expense}</td>
         </tr>
